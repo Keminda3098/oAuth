b5a7141daca0158c8de3a743fed8623eae77dcc7
PEROOT\VSAMAKE <kemindasamaraweera@pearson.com>
2018-12-22 12:28:52 +0530

    google auth setup part1

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,6 +3,8 @@ const GoogleStrategy = require('passport-google-oauth20');
 
 passport.use(new GoogleStrategy({
     //optiobs for google strat
+    clientID:'354087886803-rn6gdvgdeog67kmpnqbppret33qu8n84.apps.googleusercontent.com',
+    clientSecret:'x_wbTMDt6Cabi_zDS9mf5nGM'
 }), () => {
     //passport callback function
 });
