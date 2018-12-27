b338b525be098f6544c54afaa7ce25a43008f4f4
PEROOT\VSAMAKE <kemindasamaraweera@pearson.com>
2018-12-28 00:11:42 +0530

    saving users

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,13 +1,28 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/user-model')
 
 passport.use(new GoogleStrategy({
     //options for google strat
-    callbackURL:'/auth/google/redirect',
+    //.save retuns a promise .call back inside then wil, tel you what to do when save is finishded
+    callbackURL: '/auth/google/redirect',
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
-}, (accessToken,refreshToken,profile,done) => {
-   console.log("accessToken" + accessToken)
-   console.log("profile" + JSON.stringify(profile))
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id }).then((currentUser) => {
+        if (currentUser) {
+            //already have the user
+            console.log("User is " +currentUser.username)
+        } else {
+            new User({
+                username: profile.displayName,
+                googleId: profile.id
+            }).save().then((newUSer) => {
+                console.log('new user is ' + newUSer)
+            });
+        }
+    });
+
+
 }));
